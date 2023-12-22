05b6e1f428c05579ccd60658dd7a67f22de8c294
Jackdawson007 <154601146+Jackdawson007@users.noreply.github.com>
2023-12-22 19:53:34 +0530

    IndexAll.js

--- IndexAll.js
+++ IndexAll.js
@@ -1,15 +1,15 @@
 function getIndexAll(list, input) {
-	var findArray = list;
-	var findValue = input;
-	var firstIndex = findArray.indexOf(findValue);
-	var lastIndex = findArray.lastINdexOf(findValue);
-	var outputList = [];
+    var findArray = list;
+    var findValue = input;
+    var firstIndex = findArray.indexOf(findValue);
+    var lastIndex = findArray.lastIndexOf(findValue) + 1;
+    var outputList = [];
 
-	while (firstIndex < lastIndex) {
-		if (findValue == findArray[firstIndex]) {
-			outputList.push(firstIndex);
-		}
-		firstIndex++;
-	}
-	return outputList;
+    while (firstIndex < lastIndex) {
+        if (findValue == findArray[firstIndex]) {
+            outputList.push(firstIndex);
+        }
+        firstIndex++;
+    }
+    return outputList;
 }
